628ff80f76bff172812c533f6ac3f1809493a235
lapc18 <lapc18@outlook.com>
2023-01-18 06:55:34 -0400

    feat: add csvHelper

--- src/main/java/io/inab/atdev/payroll/core/helpers/CSVHelper.java
+++ src/main/java/io/inab/atdev/payroll/core/helpers/CSVHelper.java
@@ -1,2 +1,29 @@
-package io.inab.atdev.payroll.core.helpers;public class CSVHelper {
+package io.inab.atdev.payroll.core.helpers;
+
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.List;
+
+public class CSVHelper {
+
+    public static <T> List<T> toList(MultipartFile file, Class<T> tClass) throws Exception {
+        if(file.isEmpty() || tClass == null) return null;
+
+        try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+
+            var csvToBean = new CsvToBeanBuilder<T>(reader)
+                    .withType(tClass)
+                    .withIgnoreQuotations(true)
+                    .build();
+
+            return csvToBean.parse();
+        } catch (Exception ex) {
+            if(ex instanceof RuntimeException) throw new RuntimeException("Number of data fields does not match number of headers!");
+
+            throw new Exception(ex);
+        }
+    }
+
 }
