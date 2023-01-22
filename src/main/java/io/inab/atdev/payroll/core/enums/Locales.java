191681248278e0d1701144f104b972b833af3431
lapc18 <lapc18@outlook.com>
2023-01-22 02:09:58 -0400

    feat: add locales enum

--- src/main/java/io/inab/atdev/payroll/core/enums/Locales.java
+++ src/main/java/io/inab/atdev/payroll/core/enums/Locales.java
@@ -1,2 +1,18 @@
-package io.inab.atdev.payroll.core.enums;public enum Locales {
+package io.inab.atdev.payroll.core.enums;
+
+public enum Locales {
+
+    LOCALE_DO("do"),
+    LOCALE_US("en");
+
+    public final String locale;
+
+    private Locales(String value) {
+        this.locale = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.locale;
+    }
 }
